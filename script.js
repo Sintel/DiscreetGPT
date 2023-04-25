29824bea269a2f9f365d3f1e83f23a7778b34714
Sintel <131643199+Sintel@users.noreply.github.com>
2023-04-24 19:01:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.getElementById('chat-form');
 const mytextInput = document.getElementById('mytext');
 const responseTextarea = document.getElementById('response');
 
-const API_KEY = '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';
+const API_KEY = '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';
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
